738274c3f83483f595a4ced186d57373581a8b6d
fernicortlo <a01026613@tec.mx>
2023-03-29 17:45:54 -0600

    ahhhhh

--- TAREAS/pollito.rkt
+++ TAREAS/pollito.rkt
@@ -1,23 +1,46 @@
 ; Created by Maria Fernanda Cortés Lozano and Arantza Parra Martínez
 
-; The following code prints the lyrics of the song "El Pollito Pio" using recursion
 #lang racket
 
-;first, we define the lists of the animals and the beginning of the song
-(define animales (list "un Pollito Pio" "una Gallina Coo" "un Gallo Corocó" "un Pavo Glú Glú Glú" "una Paloma Ruu" "un Perro Guau Guau" "un Gato Miau" "una Cabra me" "un Cordero bee" " una Vaca Moo" " un Toro Muu"))
-(define animalesY (list "Y el Pollito Pio" "Y la Gallina Coo" "Y el Gallo Corocó" "Y la Paloma Ruu" "Y el Pavo Glú Glú Glú" "Y el Perro Guau Guau" "Y el Gato Miau" "Y la Cabra me" "Y el Cordero bee" "Y la Vaca Moo" "Y el Toro Muu"))
+; (define animales (list "un Pollito Pio" "una Gallina Coo" "un Gallo Coroco" "un Pavo Glu Glu Glu" "un Perro Guau Guau" "un Gato Miau" "una Cabra me" "un Cordero bee" "una Vaca Mu" "un Toro muu"))
+; (define animalesY (list "Y el Pollito Pio" "Y la Gallina Coo" "Y el Gallo Coroco" "Y el Pavo Glu Glu Glu" "Y el Perro Guau Guau" "Y el Gato Miau" "Y la Cabra me" "Y el Cordero bee" "Y la Vaca Mu" "Y el Toro muu"))
+; (define inicio (list "En la radio hay u"))
 
-(define inicio (list "En la radio hay "))
+; (define (replace-u str animal)
+;   (string-replace str "u" animal))
 
-;we define the function that will print the lyrics
-(define (pollito-lyrics inicio animales animalesla)
-  (define pollitoPio '())
-  (define agregar '())
-  (for ([i inicio])
-    (for ([x (range (length animales))])
-      (set! pollitoPio (append pollitoPio (list (string-replace i "u" (list-ref animales x)))))
-      (set! agregar (cons (list-ref animalesY x) agregar))
-      (set! pollitoPio (append pollitoPio agregar))))
-  pollitoPio)
+; (define (pollito-lyrics inicio animales animalesY)
+;   (if (null? animales)
+;       '()
+;       (let ((animal (car animales))
+;             (animalY (car animalesY)))
+;         (append (list (replace-u inicio animal))
+;                 (list "\n")
+;                 (list animalY)
+;                 (list "\n")
+;                 (pollito-lyrics inicio (cdr animales) (cdr animalesY))))))
 
-(displayln (pollito-lyrics inicio animales animalesY))
+; (define (pollitopio inicio animales animalesY )
+;   (pollito-lyrics (car inicio) animales animalesY) )
+
+; (displayln (pollitopio inicio animales animalesY))
+
+
+ 
+(define animales (list " un Pollito Pio" "una Gallina Coo" "un Gallo Coroco" "un Pavo Glu Glu Glu" "un Perro Guau Guau" "un Gato Miau" "una Cabra me" "un Cordero bee" " una Vaca Mu" " un Toro muu"))
+(define animalesla (list "Y el Pollito Pio" "Y la Gallina Coo" "Y el Gallo Coroco" "Y el Pavo Glu Glu Glu" "Y el Perro Guau Guau" "Y el Gato Miau" "Y la Cabra me" "Y el Cordero bee" " Y la Vaca Mu" " Y el Toro muu"))
+(define inicio (list "En la radio hay u"))
+
+(define (pollitopio inicio animales animalesla)
+   (define cancion '())
+   (define agregado '())
+   (for ([i inicio])
+     (for ([x (range (length animales))])
+       (set! cancion (append cancion (list (string-replace i "u" (list-ref animales x) ))))
+       
+       (set! agregado (cons (list-ref animalesla x) agregado))
+       
+       (set! cancion (append cancion agregado))))
+   cancion)
+
+ (displayln (pollitopio inicio animales animalesla))
