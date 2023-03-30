2eeafcf89509de0f80020bca761c09df4cefc309
fernicortlo <a01026613@tec.mx>
2023-03-30 11:31:04 -0600

    pollito pío done

--- TAREAS/pollito.rkt
+++ TAREAS/pollito.rkt
@@ -2,45 +2,29 @@
 
 #lang racket
 
-; (define animales (list "un Pollito Pio" "una Gallina Coo" "un Gallo Coroco" "un Pavo Glu Glu Glu" "un Perro Guau Guau" "un Gato Miau" "una Cabra me" "un Cordero bee" "una Vaca Mu" "un Toro muu"))
-; (define animalesY (list "Y el Pollito Pio" "Y la Gallina Coo" "Y el Gallo Coroco" "Y el Pavo Glu Glu Glu" "Y el Perro Guau Guau" "Y el Gato Miau" "Y la Cabra me" "Y el Cordero bee" "Y la Vaca Mu" "Y el Toro muu"))
-; (define inicio (list "En la radio hay u"))
-
-; (define (replace-u str animal)
-;   (string-replace str "u" animal))
-
-; (define (pollito-lyrics inicio animales animalesY)
-;   (if (null? animales)
-;       '()
-;       (let ((animal (car animales))
-;             (animalY (car animalesY)))
-;         (append (list (replace-u inicio animal))
-;                 (list "\n")
-;                 (list animalY)
-;                 (list "\n")
-;                 (pollito-lyrics inicio (cdr animales) (cdr animalesY))))))
-
-; (define (pollitopio inicio animales animalesY )
-;   (pollito-lyrics (car inicio) animales animalesY) )
-
-; (displayln (pollitopio inicio animales animalesY))
-
-
+;the function 'pollito-lyrics' will print the lyrics to the song 'Pollito Pio' using a recurvise function.
  
-(define animales (list " un Pollito Pio" "una Gallina Coo" "un Gallo Coroco" "un Pavo Glu Glu Glu" "un Perro Guau Guau" "un Gato Miau" "una Cabra me" "un Cordero bee" " una Vaca Mu" " un Toro muu"))
-(define animalesla (list "Y el Pollito Pio" "Y la Gallina Coo" "Y el Gallo Coroco" "Y el Pavo Glu Glu Glu" "Y el Perro Guau Guau" "Y el Gato Miau" "Y la Cabra me" "Y el Cordero bee" " Y la Vaca Mu" " Y el Toro muu"))
-(define inicio (list "En la radio hay u"))
 
-(define (pollitopio inicio animales animalesla)
-   (define cancion '())
-   (define agregado '())
-   (for ([i inicio])
-     (for ([x (range (length animales))])
-       (set! cancion (append cancion (list (string-replace i "u" (list-ref animales x) ))))
-       
-       (set! agregado (cons (list-ref animalesla x) agregado))
-       
-       (set! cancion (append cancion agregado))))
-   cancion)
 
- (displayln (pollitopio inicio animales animalesla))
+;we define the function
+(define (pollito-lyrics)
+  ;we define the lists of animals and sounds that will be used 
+  (define animals '("el Pollito Pio" "la Gallina Coo" "el Gallo Corocó" "el Pavo Glú Glú Glú" "la Paloma Ruu" "el Gato Miao" "el Perro Guau Guau" "la Cabra Mee" "el Cordero Bee" "la Vaca Moo" "el Toro Muu"))
+  (define animalsUn '("un Pollito Pio" "una Gallina Coo" "un Gallo Corocó" "un Pavo Glú Glú Glú" "una Paloma Ruu" "un Gato Miao" "un Perro Guau Guau" "una Cabra Mee" "un Cordero Bee" "una Vaca Moo" "un Toro Muu"))
+
+  ; we define a function that will help us keep track of how  we iterate throught the list called pollito-lyrics-helper
+  ; the arguments used are the lists we previously defined and a variable called 'so-far' that stores the previous animals
+  (define (pollito-lyrics-helper animals animalsUn so-far)
+    ; If there are no more animals to iterate it prints the following:
+    (cond ((null? animals)
+           (begin (display "En la radio hay un Tractor\n")
+                  (display "Y el Tractor: Bruum \n Y el Pollito: oh oh!\n")))
+          ; If there are animals in the list, it will iterate this loop until its empty
+          (else (begin (display (string-append "En la radio hay " (car animalsUn) "\n")) ; this takes and prints the current element in 'animalsUn'
+                       ;in the list according to the iteration
+                       (display (string-append "Y " (string-join (cons (car animals) so-far) " \n Y ") "\n")) ; this prints and adds the new and previous animals using 'so-far' 
+                       (pollito-lyrics-helper (cdr animals) (cdr animalsUn) (cons (car animals) so-far)))))) ; this recursively calls itself until there are no more animals 
+
+  (pollito-lyrics-helper animals animalsUn '()))
+
+(pollito-lyrics)
