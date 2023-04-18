caa2450db96cd438a9cb5cc85be38527feb6deed
fernicortlo <a01026613@tec.mx>
2023-04-18 12:14:44 -0600

    factorial

--- APUNTES/elixir/factorial.exs
+++ APUNTES/elixir/factorial.exs
@@ -4,16 +4,47 @@
 # Fer Cortés
 # 2023-04-18
 
-defmodule Factorial do
-  def fact(0), do: 1
-  def fact(n), do: n * fact(n - 1)
-end
 
+defmodule Recur do
+  def fact(n) do
+    if n == 0 do
+      1
+    else
+      n * fact(n - 1)
+    end # end if
+  end # end def
 
-def fact(n):
-    if n == 0:
-        return 1
-    else:
-        return n * fact(n - 1)
+  # using pattern maching
+  def fact_pattern(0), do: 1
+  def fact_pattern(n), do: n * fact_pattern(n - 1)
 
-print(fact(5))
+  #using 'guards'
+  def fact_guard(n) when n == 0, do: 1
+  def fact_guard(n), do: n * fact_guard(n - 1)
+
+  def fact_tail(n), do: do_fact_tail(n, 1)
+
+  # Define a private function that can only be called from within this module
+  defp do_fact_tail(n, r) do
+    if n == 0, do: r, else: do_fact_tail(n - 1, r * n)
+  end
+
+  def diploma(_grade) when grade >=90, do: :distinction
+  def diploma(_grade) when grade >=70, do: :pass
+  def diploma(_grade), do: :fail
+
+  def fibo(n) when n<0, do: :error
+  def fibo(0), do: 0
+  def fibo(1), do: 1
+  def fibo(n), do: fibo(n-1) + fibo(n-2)
+
+  #using tail recursion
+  def fibo_tail(n) when n<0, do: {:error, :negative}
+  defp fibo_tail(0) , do: {:ok, 0}
+  defp fibo_tail(1) , do: {:ok, 1}
+  def fibo_tail(n), do: do_fibo_tail(0, 1, n-1)
+
+  defp do_fibo_tail(a, b, 0), do: {:ok, b}
+  defp do_fibo_tail(a, b, n), do: do_fibo_tail(b, a+b, n-1)
+
+end # end defmodule
