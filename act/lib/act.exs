fa4d8cd063c8b75e10924914357c75a751cf3a19
fernicortlo <a01026613@tec.mx>
2023-04-30 12:59:03 -0600

    activity 2.2

---
+++ act/lib/act.exs
@@ -0,0 +1,155 @@
+#Activity 2.2 - Elixir
+# Created by Maria Fernanda Cortés Lozano and Arantza Parra
+
+defmodule Act do
+  @moduledoc """
+  Documentation for `Act`.
+  """
+
+ @doc """
+    Function that takes a number and a list and returns all possible
+    combinations of the list with the number
+
+  """
+
+  def insert-everywhere(number, list) do
+    insert-everywhere(number, list, [])
+  end
+
+  defp insert-everywhere(number, [], result) do
+    result
+  end
+
+  defp insert-everywhere(number, [head | tail], result) do
+    insert-everywhere(number, tail, [result ++ [number | head]])
+  end
+
+  @doc """
+    Function that takes a list and groups equal elements in sublists
+
+  """
+
+  def pack(list) do
+    pack(list, [], [])
+  end
+
+  defp pack([], [], result) do
+    result
+  end
+
+  defp pack([], current, result) do
+    result ++ [current]
+  end
+
+  defp pack([head | tail], [], result) do
+    pack(tail, [head], result)
+  end
+
+  defp pack([head | tail], current, result) do
+    if head == hd(current) do
+      pack(tail, [head | current], result)
+    else
+      pack(tail, [head], result ++ [current])
+    end
+  end
+
+
+  @doc """
+    A function that takes a list in ascendent order and a number and
+    returns the list with the number inserted in the correct position
+
+  """
+
+  def insert(list, number) do
+    insert(list, number, [])
+  end
+
+  defp insert([], number, result) do
+    result ++ [number]
+  end
+
+  defp insert([head | tail], number, result) do
+    if number < head do
+      result ++ [number] ++ [head] ++ tail
+    else
+      insert(tail, number, result ++ [head])
+    end
+  end
+
+  @doc """
+    Function that rotates a list to the left or to the right by a given number of positions.
+    If the number is positive, the rotation is to the left, if it is negative, the rotation is to the right.
+  """
+
+  def rotate_left(list, number) do
+    rotate_left(list, number, [])
+  end
+
+  defp rotate_left(list, 0, result) do
+    result ++ list
+  end
+
+  defp rotate_left(list, number, result) do
+    if number > 0 do
+      rotate_left(tl(list) ++ [hd(list)], number - 1, result)
+    else
+      rotate_left([last(list)] ++ init(list), number + 1, result)
+    end
+  end
+
+  @doc """
+    Function takes a number and returns a list of its prime factors
+  """
+
+  def prime_factors (number) do
+    prime_factors(number, 2, [])
+  end
+
+  @doc """
+    Function that takes two numbers greater than zero and returns the
+    greatest common divisor without using predefines functions
+
+  """
+
+  def gcd(number1, number2) do
+    if number1 == number2 do
+      number1
+    else
+      if number1 > number2 do
+        gcd(number1 - number2, number2)
+      else
+        gcd(number1, number2 - number1)
+      end
+    end
+  end
+
+  @doc """
+    Function that takes a list and returns a list of tuples with the
+    number and the number of times it appears in the list
+  """
+
+  def encode(list) do
+    encode(list, [], [])
+  end
+
+  defp encode([], [], result) do
+    result
+  end
+
+  defp encode([], current, result) do
+    result ++ [current]
+  end
+
+  defp encode([head | tail], [], result) do
+    encode(tail, [head, 1], result)
+  end
+
+  defp encode([head | tail], current, result) do
+    if head == hd(current) do
+      encode(tail, [head, tl(current) + 1], result)
+    else
+      encode(tail, [head, 1], result ++ [current])
+    end
+  end
+
+end
