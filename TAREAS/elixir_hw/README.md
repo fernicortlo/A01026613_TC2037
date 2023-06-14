a90c37b4e1e17b34da4e74142a51835d1237e0dd
fernicortlo <a01026613@tec.mx>
2023-06-14 17:12:06 -0600

    homework done

--- TAREAS/elixir_hw/README.md
+++ TAREAS/elixir_hw/README.md
@@ -1,9 +1,42 @@
 # <span style = "color: lightblue;"> Actividad 5.2 Programación paralela y concurrente </span>
 **Created by Fernanda Cortés and Arantza Parra**
 
-## <span style = "color: lightblue;"> Análisis de tiempo y Speedup </span>
+## <span style = "color: lightblue;"> Time complexity and Speedup analysis </span>
 
-### <span style = "color: lightgreen;"> Análisis de tiempo speedup</span>
+### <span style = "color: lightgreen;"> Time complexity </span>
+
+The is_prime() function has a time complexity of O(sqrt(n)), as to determine whether a number is prime, it tests all possible divisors up to the square root of the number.
+
+The sum_primes and prime_sum functions have a time complexity of O(n sqrt(n)), as it calls is_prime for each number in the range from 1 to n. That is, for each number in the range, it performs an operation that takes time proportional to the square root of the number.
+
+The sum_prime_parallel function also has a time complexity of O(n sqrt(n)), as it performs the same operations as sum_primes and prime_sum. However, because it divides the work into parallel tasks, it can complete these operations more quickly on systems with multiple processor cores.
 
 
-### <span style = "color: lightgreen;"> Speedup</span>
+### <span style = "color: lightgreen;"> Speedup </span>
+- The number of cores being used by our tests are 8.
+
+![cores](/TAREAS/elixir_hw/images/cores.png)
+
+The output of the two functions is following:
+
+![output](/TAREAS/elixir_hw/images/output.png)
+
+The output is the same on both functions. Where we can identify the difference between concurrent and parallel programs is with the time efficiency. Time efficiency also translates into better, more efficient programs in terms of resources.
+
+Using the :timer.tc functions returns the time in miliseconds. The line of code calculates the sum of all prime numbers up to 5,000,000, measures the execution time of this operation, and then converts that time from microseconds to seconds.
+Running the function three different times we will calculate an average in order to get a more real result and comparison between functions. 
+
+For the first function using concurrent programation, the time obtained is the following:
+
+![1st function time](/TAREAS/elixir_hw/images/time1st.png)
+
+The average of this first function is: 28.69 seconds.
+
+For the second function that uses parallel programing, the time obtained is:
+
+![2nd function time](/TAREAS/elixir_hw//images/time2nd.png)
+
+If we calculate the average we get 7.67 seconds. 
+
+Based on the results, we can see that parallel function is the most efficient in terms fo time consumption. There's a 21.02 second difference between them. 
+
--- TAREAS/elixir_hw/concur_hw.exs
+++ TAREAS/elixir_hw/concur_hw.exs
@@ -1,54 +1,60 @@
 defmodule Hw.Primes do
+
+  # Function that returns true if a number is prime
+  # and false otherwise.
   def is_prime(2), do: true
 
   def is_prime(n) when n < 2 do
     false
   end
-
   def is_prime(n) do
     limit = :math.sqrt(n) |> Float.ceil(0) |> round
     Enum.all?(2..limit, fn i -> rem(n, i) != 0 end)
   end
 
+  # Function that returns the sum of all primes up to n.
   def sum_primes(n) do
     Enum.filter(1..n, &is_prime/1) |> Enum.sum()
   end
 
+  # This function should return the sum of all primes between from and to.
   def prime_sum(lower, upper) do
     Enum.filter(lower..upper, &is_prime/1)
     |> Enum.sum()
   end
 
+  # This function makes the ranges used for parallelization.
   def make_ranges(start, finish) do
-    cores = System.schedulers_online()
-    total_range = finish - start + 1
-    range_size = div(total_range, cores)
-    remainder = rem(total_range, cores)
+    cores = System.schedulers_online() # Number of cores obtained from the system.
+    total_range = finish - start + 1 # Total range of numbers to be summed.
+    range_size = div(total_range, cores) # Size of each range.
+    remainder = rem(total_range, cores) # Remainder of the division.
 
-    ranges = for i <- 0..(cores - 1) do
-      lower = start + (i * range_size) + min(i, remainder)
-      upper = lower + range_size - 1 + if i < remainder, do: 1, else: 0
-      {lower, upper}
+    ranges = for i <- 0..(cores - 1) do # Create the ranges.
+      lower = start + (i * range_size) + min(i, remainder)  # Add the remainder to the first ranges.
+      upper = lower + range_size - 1 + if i < remainder, do: 1, else: 0 # Add 1 to the first ranges.
+      {lower, upper} # Return the range.
     end
 
-    List.update_at(ranges, -1, fn last -> {elem(last, 0), finish} end)
+    List.update_at(ranges, -1, fn last -> {elem(last, 0), finish} end) # Update the last range.
   end
 
-  def sum_primes_parallel(from, to) do
+  # This function should return the sum of all primes between from and to in parallel.
+  def sum_prime_parallel(from, to) do
     ranges = make_ranges(from, to)
 
     tasks =
-      for {lower, upper} <- ranges do
-        Task.async(fn -> prime_sum(lower, upper) end)
+      for {lower, upper} <- ranges do # Iterate over the ranges.
+        Task.async(fn -> prime_sum(lower, upper) end) # Create a task for each range.
       end
 
     tasks
-    |> Enum.map(&Task.await/1)
-    |> Enum.sum()
+    |> Enum.map(&Task.await/1) # Await the tasks.
+    |> Enum.sum() # Sum the results of each task.
   end
 
 
 end
 
-IO.inspect(Hw.Primes.sum_primes(5_000_000))
-IO.inspect(Hw.Primes.sum_primes_parallel(1, 5_000_000))
+IO.inspect(Hw.Primes.sum_primes(5_000_000)) # runs the concurrent version
+IO.inspect(Hw.Primes.sum_primes_parallel(1, 5_000_000)) # runs the parallel version
